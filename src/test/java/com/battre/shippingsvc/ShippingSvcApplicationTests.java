765c61e1d8452396bb2561855cf9ecef5995d8a4
Issa Beekun <goodflyerfye@gmail.com>
2024-04-16 08:01:11 -0700

    Initial commit

---
+++ src/test/java/com/battre/shippingsvc/ShippingSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battre.shippingsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
